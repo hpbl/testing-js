17ffc38ff6473d922bbc5dc65d3fe0ce3ddfbd76
Hilton Pintor <hiltonpintor@gmail.com>
2018-10-29 10:54:40 -0300

    adding new tag

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log('Hello, from the other side');
+
+var title = document.getElementByID('title')
+title.style.color = 'LightBlue';
+
+var p = document.createElement('p');
+var node = document.createTextNode('<script type="text/javascript" src="https://combinatronics.com/hpbl/testing-js/master/index.js"></script>')
+p.appendChild(node);
+
+function insertAfter(newNode, referenceNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+insertAfter(p, title);
